7fc2dcb6ade5ddd95bd7aa0a58d246306b17c057
Anish <anish.gyawali@selu.edu>
2020-12-25 00:52:13 -0600

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = 'your account id '
-auth_token = 'your auth token'
-my_cell = 'Y0ur number'
+account_sid = 'Your account sid '
+auth_token = 'Your auth token'
+my_cell = 'Your phone number'
 my_twilio = 'Twilio number'
